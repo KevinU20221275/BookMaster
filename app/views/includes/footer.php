56321175747aabcbdac79748437c6e0acfc042e3
KevinU20221275 <U20221275@univo.edu.sv>
2025-04-24 14:05:46 -0600

    separete parts of template on includes directory

---
+++ app/views/includes/footer.php
@@ -0,0 +1,17 @@
+<footer class="footer">
+    <div class="container-fluid d-flex justify-content-between">
+        <nav class="pull-left">
+            <ul class="nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        Book Master
+                    </a>
+                </li>
+            </ul>
+        </nav>
+        <div class="copyright">
+            2024, made by
+            <a href="#">Book Master</a>
+        </div>
+    </div>
+</footer>
